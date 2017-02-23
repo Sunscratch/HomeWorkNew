a6f07ff9154f2707b7594b28b99b8c236652de51
Alexander Bazhanov <alexander.b1982@gmail.com>
2017-02-23 14:55:01 +0200

    insert and swim ready, not tested

--- homework/lesson_17/alexander.bazhanov/src/main/java/algo/PriorityQueue.java
+++ homework/lesson_17/alexander.bazhanov/src/main/java/algo/PriorityQueue.java
@@ -16,7 +16,13 @@ public class PriorityQueue<T extends Comparable> {
   }
 
   public void insert (T item) {
-
+    if (size == array.length) {
+      Object[] newArray = new Object[array.length * 2];
+      System.arraycopy(array,0, newArray,0, array.length);
+      array = newArray;
+    }
+    array[size++] = item;
+    swim(size);
   }
 
   public T min() {
@@ -24,16 +30,19 @@ public class PriorityQueue<T extends Comparable> {
   }
 
   private void swim(int i) {
-
+    while(!isRoot(i) && lessOrEqual(i, getParentIndex(i))){
+      exchange(i, getParentIndex(i));
+      i = getParentIndex(i);
+    }
   }
 
   private void sink(int i){
 
   }
-
+  @SuppressWarnings("unchecked")
   private boolean lessOrEqual(int first, int second) {
     if (comparator == null) {
-      return ((Comparable)array[first]).compareTo(array[second]) < 1;
+      return ((Comparable)array[first]).compareTo(array[second]) < 1 ;
     } else {
       return (comparator.compare((T)array[first], (T)array[second])) < 1;
     }
@@ -41,7 +50,11 @@ public class PriorityQueue<T extends Comparable> {
   }
 
   private boolean isRoot(int i) {
-    return i == 0;
+    return i == 1;
+  }
+
+  private int getParentIndex (int i) {
+    return i / 2;
   }
 
   private int getLeftChildIndex(int i) {
@@ -57,6 +70,13 @@ public class PriorityQueue<T extends Comparable> {
       return getRightChildIndex(i);
     }
     return getLeftChildIndex(i);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void exchange(int i, int k) {
+    T temp = (T) array[i];
+    array[i] = array[k];
+    array[k] = temp;
   }
 
 }
